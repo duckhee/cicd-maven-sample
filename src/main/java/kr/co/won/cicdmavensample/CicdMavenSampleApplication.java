4bdf9bbb8e411029fd06d5bb727d2500f4b90c53
DoukheeWon <fain9301@yahoo.com>
2022-10-23 01:19:04 +0900

    feat: first commit

---
+++ src/main/java/kr/co/won/cicdmavensample/CicdMavenSampleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.won.cicdmavensample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdMavenSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdMavenSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/won/cicdmavensample/CicdMavenSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.won.cicdmavensample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdMavenSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
