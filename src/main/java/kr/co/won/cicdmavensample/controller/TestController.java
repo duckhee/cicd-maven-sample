3dffe956bd18822eac42e4b137ec40313d925b04
doukheeWon <fain9301@candh.co.kr>
2022-10-24 18:53:44 +0900

    feat: change test

--- src/main/java/kr/co/won/cicdmavensample/controller/TestController.java
+++ src/main/java/kr/co/won/cicdmavensample/controller/TestController.java
@@ -2,9 +2,11 @@ package kr.co.won.cicdmavensample.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(path = "/")
 public class TestController {
 
     @GetMapping
